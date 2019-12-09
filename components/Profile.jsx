000a8d7c45f7198f838e192d516169c8905f0249
roman <roman-1901@mail.ru>
2019-12-09 21:57:25 +0300

    added new files

---
+++ components/Profile.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+                <div>
+                    <img src='https://s1.1zoom.me/big0/994/260356-svetik.jpg'></img>
+                </div>
+                <div>
+                    ava + description
+                </div>
+                <div>
+                    My posts
+                    <div>
+                        New post
+                    </div>
+                    <div>
+                        post1
+                    </div>
+                    <div>
+                        post2
+                    </div>
+                </div>
+             </div>
+}
+
+export default Profile;

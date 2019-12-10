5377622526b4b1583889c754cff2e71336fc4645
roman <roman-1901@mail.ru>
2019-12-10 23:28:12 +0300

    test

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -8,6 +8,7 @@ const Navbar = () => {
                 <div><a href='#'>News</a></div>
                 <div><a href='#'>Music</a></div>
                 <div><a href='#'>Settings</a></div>
+                <div><a href='#'>Testing</a></div>
             </nav>
 }
 
